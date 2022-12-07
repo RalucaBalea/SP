ea6154343873cdb7873e68dacc7d96af51d94f2c
Balea Raluca <raluca.balea02@e-uvt.ro>
2022-12-07 10:04:26 +0200

    Document Manager

--- src/SPproiect/DocumentManager.java
+++ src/SPproiect/DocumentManager.java
@@ -1,5 +1,27 @@
 package SPproiect;
 
 public class DocumentManager {
-
+	
+	 private static DocumentManager instance = new DocumentManager();
+	 private Book myBook;
+	 
+	 public static DocumentManager getInstance(){
+	      return instance;
+	   }
+	 
+	public Book getBook() {
+		return this.myBook;
+	}
+	
+	public void setBook() {
+		this.myBook = myBook;
+	}
+	
+	public void print() {
+		
+	}
+	
+	
+	
+	 
 }
